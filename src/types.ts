8b86a7d04a1723726636353866d06ec38cf3caab
kizataka <chongxiongmu@gmail.com>
2024-07-23 09:45:06 +0900

    登録フォームの型を定義

--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,10 @@ export interface LoginFormValues {
     email: string;
     password: string;
 }
+
+export interface RegisterFormValues {
+    name: string;
+    email: string;
+    password: string;
+    password_confirmation: string;
+}
